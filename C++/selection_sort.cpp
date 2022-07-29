df6bf39c9bac99006718bd2028a9a6530b98ca34
Vayun Ekbote <91115665+vayunekbote02@users.noreply.github.com>
2022-07-29 18:25:23 +0530

    Add files via upload

---
+++ C++/selection_sort.cpp
@@ -0,0 +1,32 @@
+// Selection sort in C++
+#include <bits/stdc++.h>
+using namespace std;
+
+void selectionSort(int a[], int n){
+    for (int i=0; i<(n-1); i++){
+        for (int j=i+1; j<n; j++){
+            if (a[j] < a[i]) {
+                int temp = a[i];
+                a[i] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+    for (int i=0; i<n; i++){
+        cout << a[i] << " ";
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int a[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i< n; i++) {
+        cin >> a[i];
+    }
+    selectionSort(a, n);
+    
+    return 0;
+}
